4d93fb5bf8111280f1e5e6b878c7c9da8487655d
Sam Bower <sjb00020@mix.wvu.edu>
2024-02-14 01:36:23 -0700

    reverting to map

---
+++ assets/js/map.js
@@ -0,0 +1,5 @@
+var myMap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+}).addTo(myMap);
